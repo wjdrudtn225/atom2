d56e67d1173db003b8e2274f8a25186595230f35
wjdrudtn225 <wjdrudtn225@naver.com>
2018-05-17 11:50:22 +0900

    reactex!!

--- ajaxex/src/components/index.js
+++ ajaxex/src/components/index.js
@@ -1,6 +1,8 @@
 import Header from './Header/Header';
 import PostWrapper from './PostWrapper/PostWrapper';
+import Navigate from './Navigate/Navigate';
 export {
   Header,
-  PostWrapper
+  PostWrapper,
+  Navigate
 };
